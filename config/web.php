85e777be478bd7e3f3dcde99dc300f09dd09e053
Roelof Ruis <roelof@maester.com>
2025-11-12 22:08:46 +0100

    Example env

--- config/web.php
+++ config/web.php
@@ -1,7 +1,6 @@
 <?php
 
 $db = require __DIR__ . '/db.php';
-$base_url = getenv('BASE_URL') ?: '';
 
 $config = [
     'id' => 'basic',
@@ -15,7 +14,6 @@ $config = [
     'components' => [
         'request' => [
             'cookieValidationKey' => 'Ifhd4CldUB83y_a3ejyLAQcUk3Q9GkD6',
-            'baseUrl' => $base_url,
         ],
         'urlManager' => [
             'class' => 'yii\web\UrlManager',
