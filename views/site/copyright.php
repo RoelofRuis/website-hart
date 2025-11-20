87dd5881d2ebcba55556ab88c0d092bc642ce980
Roelof Ruis <roelof.ruis@gmail.com>
2025-11-20 14:30:54 +0100

    cleaned up translations

--- views/site/copyright.php
+++ views/site/copyright.php
@@ -9,7 +9,7 @@ $this->title = Yii::t('app', 'Copyright');
 <div class="site-static">
     <h1 class="mb-3"><?= Html::encode($this->title) ?></h1>
     <p>
-        <?= Html::encode(Yii::t('app', 'All content on this website is copyrighted by HART Music School unless otherwise stated.')) ?>
+        Alle inhoud op deze website is auteursrechtelijk beschermd door HART Muziekschool, tenzij anders vermeld.
     </p>
     <p class="text-muted small mb-0">&copy; <?= date('Y') ?> HART</p>
 </div>
