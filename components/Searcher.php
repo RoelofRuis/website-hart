fadcd3b7b4ddb67d51976fa14a809f519cbe99f9
Roelof Ruis <roelof.ruis@gmail.com>
2025-12-28 02:13:05 +0100

    fix the searcher

--- components/Searcher.php
+++ components/Searcher.php
@@ -30,23 +30,13 @@ class Searcher
             ->select("*")
             ->from(['data' => $data_query])
             ->limit($form->per_page + 1)
-            ->offset($form->getOffset());
-
-        if ($form->hasEmptyQuery()) {
-            Yii::$app->db->createCommand("SELECT setseed(:seed)", [':seed' => $this->getSearchSeed()])->execute();
-            $query->orderBy(new Expression('RANDOM()'));
-        } else {
-            Yii::$app->db->createCommand("SELECT setseed(:seed)", [':seed' => $this->getSearchSeed()])->execute();
-            $query->orderBy([
+            ->offset($form->getOffset())
+            ->orderBy([
                 new Expression('CASE WHEN type = \'static\' THEN 1 ELSE 0 END'),
                 new Expression('RANDOM()')
             ]);
-        }
 
-        if (!$form->hasEmptyQuery()) {
-            $query->addParams([':q' => $form->getTrimmedQuery()]);
-        }
-
+        Yii::$app->db->createCommand("SELECT setseed(:seed)", [':seed' => $this->getSearchSeed()])->execute();
         $rows = $query->all();
 
         $results = [];
@@ -60,7 +50,6 @@ class Searcher
             $type = (string)$row['type'];
             $title = (string)$row['title'];
             $slug = (string)$row['slug'];
-            $rank = (float)$row['rank'];
             $snippet = (string)($row['snippet'] ?? '');
             $image = isset($row['image']) ? (string)$row['image'] : null;
 
@@ -80,7 +69,6 @@ class Searcher
                 'url' => $url,
                 'snippet' => $snippet,
                 'image' => $image,
-                'rank' => $rank,
             ];
         }
 
@@ -97,28 +85,19 @@ class Searcher
                 'cn.name AS title',
                 'cn.slug AS slug',
                 'cn.cover_image AS image',
+                'cn.summary AS snippet'
             ])
+            ->andFilterWhere(['cn.category_id' => $form->category_id])
             ->from(['cn' => '{{%course}}']);
 
-        if ($form->category_id !== null) {
-            $subquery->andWhere(['cn.category_id' => $form->category_id]);
-        }
-
-        if ($form->hasEmptyQuery()) {
-            $subquery
-                ->addSelect([
-                    new Expression("1 AS rank"),
-                    'cn.summary AS snippet'
-                ]);
-        } else {
-            $subquery
-                ->addSelect([
-                    new Expression("1 AS rank"),
-                    'cn.summary AS snippet'
-                ])
-                ->innerJoin(['ct' => '{{%course_tag}}'], 'ct.course_id = cn.id')
-                ->innerJoin(['t' => '{{%tag}}'], 't.id = ct.tag_id')
-                ->andWhere(['LIKE', 't.name', new Expression(':q')]);
+        if (!$form->hasEmptyQuery()) {
+            $subquery->innerJoin(['tags' => (new Query)
+                ->select('course_id')
+                ->distinct()
+                ->from(['ct' => '{{%course_tag}}'])
+                ->innerJoin(['tg' => '{{%tag}}'], 'tg.id = ct.tag_id')
+                ->where("tg.name ILIKE :q", [':q' => '%' . $form->getTrimmedQuery() . '%'])
+            ], 'tags.course_id = cn.id');
         }
 
         return $subquery;
@@ -132,29 +111,19 @@ class Searcher
                 'u.full_name AS title',
                 't.slug AS slug',
                 't.profile_picture AS image',
+                't.description AS snippet',
             ])
             ->from(['t' => '{{%teacher}}'])
             ->innerJoin(['u' => '{{%user}}'], 't.user_id = u.id');
 
-        if ($form->hasEmptyQuery()) {
-            $subquery
-                ->addSelect([
-                    new Expression("1 AS rank"),
-                    't.description AS snippet'
-                ]);
-        } else {
-            $subquery
-                ->addSelect([
-                    new Expression("1 AS rank"),
-                    't.description AS snippet'
-                ])
-                ->innerJoin(['tt' => '{{%teacher_tag}}'], 'tt.teacher_id = t.id')
+        if (!$form->hasEmptyQuery()) {
+            $subquery->innerJoin(['tags' => (new Query)
+                ->select('teacher_id')
+                ->distinct()
+                ->from(['tt' => '{{%teacher_tag}}'])
                 ->innerJoin(['tg' => '{{%tag}}'], 'tg.id = tt.tag_id')
-                ->andWhere([
-                    'OR',
-                    ['LIKE', 'tg.name', new Expression(':q')],
-                    ['LIKE', 'u.full_name', new Expression(':q')]
-                ]);
+                ->where("tg.name ILIKE :q", [':q' => '%' . $form->getTrimmedQuery() . '%'])
+            ], 'tags.teacher_id = t.id');
         }
 
         return $subquery;
@@ -168,23 +137,18 @@ class Searcher
                 'sc.title AS title',
                 'sc.slug AS slug',
                 'sc.cover_image AS image',
+                'sc.summary AS snippet',
             ])
             ->from(['sc' => '{{%static_content}}']);
 
-        if ($form->hasEmptyQuery()) {
-            $subquery->addSelect([
-                new Expression('0 AS rank'),
-                'sc.summary AS snippet',
-            ]);
-        } else {
-            $subquery
-                ->addSelect([
-                    new Expression('0 AS rank'),
-                    'sc.summary AS snippet',
-                ])
-                ->innerJoin(['sct' => '{{%static_content_tag}}'], 'sct.static_content_id = sc.id')
+        if (!$form->hasEmptyQuery()) {
+            $subquery->innerJoin(['tags' => (new Query)
+                ->select('static_content_id')
+                ->distinct()
+                ->from(['sct' => '{{%static_content_tag}}'])
                 ->innerJoin(['tg' => '{{%tag}}'], 'tg.id = sct.tag_id')
-                ->andWhere(['LIKE', 'tg.name', new Expression(':q')]);
+                ->where("tg.name ILIKE :q", [':q' => '%' . $form->getTrimmedQuery() . '%'])
+            ], 'tags.static_content_id = sc.id');
         }
 
         return $subquery;
@@ -192,7 +156,7 @@ class Searcher
 
     private function getSearchSeed(): float
     {
-        $seed = Yii::$app->session->get(self::SEARCH_SEED, null);
+        $seed = Yii::$app->session->get(self::SEARCH_SEED);
         if ($seed === null) {
             $seed = mt_rand() / mt_getrandmax();
             Yii::$app->session->set(self::SEARCH_SEED, $seed);
