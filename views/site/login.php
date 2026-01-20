c68ebff3659bc2f00c76eeeadde2ef2f18214eb2
Roelof Ruis <roelof@maester.com>
2026-01-20 14:11:58 +0100

    login background

--- views/site/login.php
+++ views/site/login.php
@@ -8,25 +8,29 @@ use yii\bootstrap5\Html;
 $this->title = Yii::t('app', 'Login');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login row justify-content-center">
-    <div class="col-md-6">
-        <h1><?= Html::encode($this->title) ?></h1>
+<div class="site-login justify-content-center">
+    <div class="card">
+        <div class="card-body row">
+            <div class="col-md-6">
+                <h1><?= Html::encode($this->title) ?></h1>
 
-        <p><?= Yii::t('app','Please fill out the following fields to login:'); ?></p>
+                <p><?= Yii::t('app','Please fill out the following fields to login:'); ?></p>
 
-        <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+                <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
-        <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
+                <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
 
-        <?= $form->field($model, 'password')->passwordInput() ?>
+                <?= $form->field($model, 'password')->passwordInput() ?>
 
-        <div class="form-group">
-            <?= Html::submitButton(Yii::t('app', 'Login'), ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                <div class="form-group">
+                    <?= Html::submitButton(Yii::t('app', 'Login'), ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                </div>
+
+                <?php ActiveForm::end(); ?>
+            </div>
+            <div class="col-md-6">
+
+            </div>
         </div>
-
-        <?php ActiveForm::end(); ?>
-    </div>
-    <div class="col-md-6">
-
     </div>
 </div>
